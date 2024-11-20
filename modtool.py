1ddaacdb0e18025379e12aeacaca5e3d9dc91801
buzz <buzz@buzz.com>
2024-11-20 14:37:44 +0900

    created config file, PIP compliance, other restructuring

--- modtool.py
+++ modtool.py
@@ -1,34 +1,37 @@
 import requests
 import csv
+import json
 import argparse
 import time
+from pathlib import Path
 import concurrent.futures
 
-class modrinthAPI:
-    def __init__(self):
+class ModrinthAPI:
+    def __init__(self, config):
         self.API = "https://api.modrinth.com/v2/"
-        self.reqCount = 0
-        self.maxCalls = 300
-        self.accessTime = time.time_ns()
+        self.reqcount = 0
+        self.maxcalls = 300
+        self.accesstime = time.time_ns()
         self.minute = 60 * 10 ** 9
+        self.config = config
 
-    def rateLimit(self):
-        self.reqCount += 1
-        if self.reqCount == 1:
-            self.accessTime = time.time_ns()
-        elapsed = time.time_ns() - self.accessTime
-        if elapsed < self.minute and self.reqCount >= self.maxCalls:
+    def ratelimit(self):
+        self.reqcount += 1
+        if self.reqcount == 1:
+            self.accesstime = time.time_ns()
+        elapsed = time.time_ns() - self.accesstime
+        if elapsed < self.minute and self.reqcount >= self.maxcalls:
             print("Rate limited... pausing")
-            self.reqCount = 0
+            self.reqcount = 0
             time.sleep(self.minute - elapsed)
             print("resumed")
         elif elapsed >= self.minute:
-            self.reqCount = 0
+            self.reqcount = 0
 
-    def get(self, url):
+    def get(self, url, params=''):
         try:
-            self.rateLimit()
-            r = requests.get(self.API + url)
+            self.ratelimit()
+            r = requests.get(self.API + url, params=params)
             r.raise_for_status()
             return r.json()
         except requests.exceptions.RequestException as err:
@@ -36,33 +39,56 @@ class modrinthAPI:
 
     def download(self, url, path):
         try:
-            self.rateLimit()
+            self.ratelimit()
             c = requests.get(url)
             c.raise_for_status()
             open(path, "xb").write(c.content)
         except requests.exceptions.RequestException as err:
             raise err
 
-    def version(self, modID):
-        url = f'project/{modID}/version'
-    def checkDependencies(self, modID):
-        url = f'project/{modID}/dependencies'
+    def fetch_versions(self, mod_id, game_version=None):
+        if game_version == None:
+            game_version = self.config['game_version']
+        filter = {
+            "game_version": [game_version],
+            "loaders": ["fabric"]
+        }
+        url = f'project/{mod_id}/version'
+        return self.get(url, filter)
+    
+    def check_dependencies(self, mod_id):
+        url = f'project/{mod_id}/dependencies'
         res = self.get(url)
-        dependencies = []
-        for dep in res["projects"]:
-            dependencies.append(dep["project_id"])
-        return dependencies
+        return [dep['id'] for dep in res['projects']]
+    
+    def get_mod(self, name, game_version=None):
+        if game_version == None:
+            game_version = self.config['game_version']
+        res = self.get(f'project/{name}')
+        id = res['id']
+        latest = self.fetch_versions(id, game_version)[0]
+        dependencies = self.check_dependencies(id)
+        return {
+            "name": name,
+            "project_id": id,
+            "version_id": latest['id'],
+            "date": latest['date_published'],
+            "dependencies": dependencies
+        }
 
-modAPI = modrinthAPI()
+with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+# TODO: Pass optional arguments to config
+modAPI = ModrinthAPI(config)
 
-def parseArgs():
+def parse_args():
     parser = argparse.ArgumentParser(
         prog="modtools",
         description="A CLI tool to automatically download and maintain Minecraft mods from Modrinth",
     )
-    parser.add_argument("listPath",
+    parser.add_argument("list_path",
                         help="Path to CSV list containing mods")
-    parser.add_argument("-p", "--modpath",
+    parser.add_argument("-p", "--mod_path",
                         default="./mods",
                         help="Path to download mods to")
     parser.add_argument("-v", "--version",
@@ -72,42 +98,57 @@ def parseArgs():
 
     return parser.parse_args()
 
-args = parseArgs()
+args = parse_args()
+
+def prompt(q):
+    while True:
+        i = input(f'{q} (Y/n): ')
+        if i == 'Y':
+            return True
+        elif i == 'n':
+            return False
 
 
-def parseUserModList(path):
-    prefixA = "https://modrinth.com/mod/"
-    prefixB = "https://modrinth.com/datapack/"
-    columnModrinthLink = 4
-    columnServerSide = 8
-    modList = []
+def parse_user_list(path):
+    prefix_a = "https://modrinth.com/mod/"
+    prefix_b = "https://modrinth.com/datapack/"
+    column_modrinth_link = 4
+    column_server_side = 8
+    modlist = []
     with open(path, "r", newline='') as csvfile:
         csvreader = csv.reader(csvfile)
         for row in csvreader:
-            if row[columnServerSide] == "TRUE":
-                modName = row[columnModrinthLink].replace(prefixA, '').replace(prefixB, '')
-                modList.append(modName)
-                modList = list(filter(None, modList))
-    return modList
+            if row[column_server_side] == "TRUE":
+                modName = row[column_modrinth_link].replace(prefix_a, '').replace(prefix_b, '')
+                modlist.append(modName)
+                modlist = list(filter(None, modlist))
+    return modlist
 
-def idfyUserModList(modList):
-    ids = []
+def fetch_user_list(modlist):
+    mods = []
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
-        for mod in modList:
-            futures.append(executor.submit(modAPI.get, url=f'project/{mod}'))
+        for mod in modlist:
+            futures.append(executor.submit(modAPI.get_mod, name=mod))
+            print(f'fetching mod {mod}...')
         for future in concurrent.futures.as_completed(futures):
-            id = future.result()["id"]
-            ids.append(id)
+            res_mod = future.result()
+            mods.append(res_mod)
+            print(f'fetched mod {res_mod['name']}')
         
-    return ids
-
-def loadMods(idList):
-    pass
+    return mods
 
 
-# TODO: Create generated list file for faster access, only load CSV if file is missing
 
-collection = idfyUserModList(parseUserModList(args.listPath))
-print(collection)
-print(modAPI.reqCount)
+def main():
+    modlist_file = Path('modlist.json')
+    if modlist_file.is_file():
+        with modlist_file.open('r') as f:
+            modlist = json.load(f)
+    elif prompt("Modlist not located, fetch mods?"):
+        modlist = fetch_user_list(parse_user_list(args.list_path))
+        print(modAPI.reqcount)
+        # print(getMod(collection[0]), args.version)
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -5,4 +5,4 @@ r = requests.get(API + 'project/diagonal-fences/dependencies')
 body = r.json()
 
 # print(body["projects"][0])
-print(body["versions"][0]["project_id"])
+print(body["versions"][0])
